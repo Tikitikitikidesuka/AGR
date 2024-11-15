0f19f09a50f3a69e00add94f23baf68b7cb16d50
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2024-11-15 20:46:44 +0100

    Finish network config copying beta

--- P2/deploy.py
+++ P2/deploy.py
@@ -41,25 +41,36 @@ def create_vm(machine: Machine, config: Configuration):
     create_xml_file(machine, config)
     create_image_file(machine, config)
 
+    xml_path = Path(config.general.xml_output_dir) / machine_xml_filename(machine.name)
+    image_path = Path(config.general.disk_output_dir) / machine_img_filename(machine.name)
+    network_files_dir = Path(config.general.network_config_output_dir)
+
     # Register vm to virsh
-    os.system(f"sudo virsh define {machine_xml_filename(machine.name)}")
+    os.system(f"sudo virsh define {xml_path}")
 
     # Pasos importantes que se me van a olvidar:
     # 1. Pasar el fichero interface de cada máquina a /etc/network/interfaces
     # 2. Pasar los ficheros link de cada máquina a /etc/systemd/network/{nombrefichero}.link
 
-    # Copy interfaces config
-    pass
+    # Copy network config files to the machine
+    for file in os.listdir(network_files_dir / machine.name):
+        file_path = os.path.join(network_files_dir / machine.name, file)
+        if os.path.isfile(file_path) and file.endswith(LINK_FILE_SUFFIX):
+            os.system(f"sudo virt-copy-in -a {image_path} {file_path} /etc/systemd/network/")
+        elif os.path.isfile(file_path) and file.endswith(INTERFACE_FILE_SUFFIX):
+            os.system(f"sudo virt-copy-in -a {image_path} {file_path} /etc/network/interfaces")
+
+    # Pasar las llamadas de os.system a subprocess !!!
 
 
 def main():
     config = load_configuration(CONFIG_FILE)
-    output_dir = Path(config.general.network_config_output_dir) / machine_sanitez_name(config.routers[-1].name)
+    """output_dir = Path(config.general.network_config_output_dir) / machine_sanitez_name(config.routers[-1].name)
     output_dir.mkdir(parents=True, exist_ok=True)
     NetworkGenerator.write_network_config_files(config.routers[0], str(output_dir), INTERFACE_FILE_SUFFIX, LINK_FILE_SUFFIX)
-    create_xml_file(config.routers[0], config)
-    #for machine in config.servers + config.routers:
-        #create_vm(machine, config)
+    create_xml_file(config.routers[0], config)"""
+    for machine in config.servers + config.routers:
+        create_vm(machine, config)
 
 
 if __name__ == '__main__':

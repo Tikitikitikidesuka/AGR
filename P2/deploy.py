6a37c24fc49d235a5e561d7cb28b4ae9b51cc542
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2024-11-15 13:23:51 +0100

    Update

--- P2/deploy.py
+++ P2/deploy.py
@@ -1,21 +1,41 @@
+import os
 from pathlib import Path
 from config_reader import load_configuration, Configuration
 from vm_xml_builder import VmXmlBuilder
 
+def machine_img_filename(name: str):
+    IMAGE_SUFFIX = "-img.qcow2"
+    sanitized_name = name.lower().replace(' ', '')
+    return f"{sanitized_name}{IMAGE_SUFFIX}"
+
+def machine_xml_filename(name: str):
+    IMAGE_SUFFIX = "-vm.xml"
+    sanitized_name = name.lower().replace(' ', '')
+    return f"{sanitized_name}{IMAGE_SUFFIX}"
+
 def create_xml_files(config: Configuration):
     for item in config.servers + config.routers:
-        XML_SUFFIX = "-vm.xml"
-        IMAGE_SUFFIX = "-img.qcow"
-
-        sanitized_name = item.name.lower().replace(' ', '')
-
         VmXmlBuilder.from_template(config.general.xml_template_path) \
             .name(item.name) \
             .bridges([interface.bridge for interface in item.interfaces]) \
-            .image_path(str(Path(config.general.base_disk_path).parent / f"{sanitized_name}{IMAGE_SUFFIX}")) \
-            .output_path(str(Path(config.general.xml_output_dir) / f"{sanitized_name}{XML_SUFFIX}")) \
+            .image_path(str(Path(config.general.base_disk_path).parent / machine_img_filename(item.name))) \
+            .output_path(str(Path(config.general.xml_output_dir) / machine_xml_filename(item.name))) \
             .build()
+
+def create_images(config: Configuration):
+    for server in config.servers:
+        # Create new image file
+        os.system(f"qemu-img create -f qcow2 -b {config.general.base_disk_path} -F qxow2 {machine_img_filename(server.name)}")
+        # Register vm to virsh
+        os.system(f"sudo virsh define {machine_xml_filename(server.name)}")
+        # Copy interfaces config
+
+
+    for router in config.routers:
+        pass
+
 
 if __name__ == '__main__':
     config = load_configuration("config.toml")
     create_xml_files(config)
+    create_images(config)
